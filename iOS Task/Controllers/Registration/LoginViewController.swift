b2962ec38804c33c98f48f3b7bd32c9aca1cfd15
Mohammed Mohsin <31709209+MohammedMohsin1616@users.noreply.github.com>
2018-10-20 18:31:58 +0200

    Update LoginViewController.swift

--- iOS Task/Controllers/Registration/LoginViewController.swift
+++ iOS Task/Controllers/Registration/LoginViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  iOS Task
 //
-//  Created by Mohsin on 16.10.18.
-//  Copyright © 2018 Mohammed Mohsin. All rights reserved.
+//  Created by Mohammed Mohsin Sayed on 10/16/18.
+//  Copyright © 2018 Mohammed Mohsin Sayed. All rights reserved.
 //
 
 import UIKit
